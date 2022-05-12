eb3491ffb7b85e33bc33b4bd334df0b9485d6f63
Bohdan <b.yaronnyi@gmail.com>
2022-05-12 21:32:34 +0300

    added App.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import ReactDom from 'react-dom'
+import App from './container/App.js'
 
-// Реакт КОМПОНЕНТ
-
-const Title = ({ text, id }) => {
-    return (
-        <h1>
-            Hello {text}, {id}
-        </h1>
-    )
-}
-
-Title.propTypes = {
-    text: PropTypes.string,
-    id: PropTypes.number.isRequired,
-}
-
-Title.defaultProps = {
-    text: 'World',
-}
-
-// Реакт ФРАГМЕНТ
-const List = () => {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-    )
-}
-
-const el = (
-    <>
-        <Title text="Jack" id={1} />
-        <Title id={2} />
-        <List />
-    </>
-)
-
-ReactDom.render(el, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'))
