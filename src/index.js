b06b18f063bb9d290bb4066d54b5d80ac9d87755
Bohdan <b.yaronnyi@gmail.com>
2022-05-12 12:26:22 +0300

    added test components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Title() {
-    return <h1>Hello World</h1>
+const Title = (props) => {
+    return <h1>Hello {props.text}</h1>
+}
+
+const List = () => {
+    return (
+        <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ul>
+    )
 }
 
 const el = (
     <div>
-        <Title />
+        <Title text="Jack" />
+        <Title text="John" />
+        <List />
     </div>
 )
 
