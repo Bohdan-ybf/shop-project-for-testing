a220fb2b200993c76948b5600e5d08ab45516dbe
Bohdan <b.yaronnyi@gmail.com>
2022-05-12 22:04:51 +0300

    installed material UI

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 const App = () => {
     return (
         <>
             <h1>Hello App.js</h1>
+            <Button variant="contained">Send</Button>
         </>
     )
 }
